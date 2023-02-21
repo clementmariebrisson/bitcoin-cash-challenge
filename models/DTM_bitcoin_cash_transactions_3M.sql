1a146d237c67dbf6556a7978d460481d8a90b9bb
clementmariebrisson <clement.marie-brisson2.auditeur@lecnam.net>
2023-02-22 00:32:05 +0100

    CMB|20230222|ALIM DTM

---
+++ models/DTM_bitcoin_cash_transactions_3M.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table', table_name='STG_bitcoin_cash_transactions_3M') }}
+
+SELECT
+	ARRAY_TO_STRING(inputs_addresses, ",") as inputs_addresses,
+	ARRAY_TO_STRING(outputs_addresses, ",") as outputs_addresses,
+	SUM(input_value) as input_balance,
+	SUM(output_value) as output_balance,
+	DATE(block_timestamp) as block_date
+FROM `bitcoin-cash-challenge.dbt_cmariebrisson.STG_bitcoin_cash_transactions_3M`
+WHERE is_coinbase = FALSE
+GROUP by 
+	inputs_addresses,
+	outputs_addresses,
+	block_date
+ORDER by 
+	input_balance desc,
+	output_balance desc,
+	block_date desc
