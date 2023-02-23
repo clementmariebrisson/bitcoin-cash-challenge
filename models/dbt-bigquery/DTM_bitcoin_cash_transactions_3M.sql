f0c2fcab6751dd9f7de38f5f7253cff5605be979
clementmariebrisson <clement.marie-brisson2.auditeur@lecnam.net>
2023-02-24 00:36:45 +0100

    CMB|20230224|SQLFluff test on models/DTM_.sql

--- models/dbt-bigquery/DTM_bitcoin_cash_transactions_3M.sql
+++ models/dbt-bigquery/DTM_bitcoin_cash_transactions_3M.sql
@@ -3,7 +3,7 @@
 -- THIS REQUEST CREATES THE STAGING TABLE
 
 
-SEELECT
+SEEEEEELECT
 	ARRAY_TO_STRING(inputs_addresses, ",") as inputs_addresses,
 	ARRAY_TO_STRING(outputs_addresses, ",") as outputs_addresses,
 	SUM(input_value) as input_balance,
