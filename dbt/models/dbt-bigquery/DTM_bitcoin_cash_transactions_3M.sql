bdefa46934e550e2666e16e60cec4616293bcfcf
clementmariebrisson <clement.marie-brisson2.auditeur@lecnam.net>
2023-02-24 01:52:36 +0100

    CMB|20230224|Indentation modification

--- dbt/models/dbt-bigquery/DTM_bitcoin_cash_transactions_3M.sql
+++ dbt/models/dbt-bigquery/DTM_bitcoin_cash_transactions_3M.sql
@@ -1,19 +1,18 @@
 {{ config(materialized='table', table_name='DTM_bitcoin_cASh_transactions_3M') }}
 -- THIS REQUEST CREATES THE STAGING TABLE
 SELECT
-	ARRAY_TO_STRING(inputs_addresses, ",") AS inputs_addresses,
-	ARRAY_TO_STRING(outputs_addresses, ",") AS outputs_addresses,
-	SUM(input_value) AS input_balance,
-	SUM(output_value) AS output_balance,
-	DATE(block_timestamp) AS block_date
+    ARRAY_TO_STRING(inputs_addresses, ",") AS inputs_addresses,
+    ARRAY_TO_STRING(outputs_addresses, ",") AS outputs_addresses,
+    SUM(input_value) AS input_balance,
+    SUM(output_value) AS output_balance,
+    DATE(block_timestamp) AS block_date
 FROM `bitcoin-cASh-challenge.dbt_cmariebrisson.STG_bitcoin_cASh_transactions_3M`
 WHERE is_coinbASe = FALSE
 GROUP BY 
-	inputs_addresses,
-	outputs_addresses,
-	block_date
+    inputs_addresses,
+    outputs_addresses,
+    block_date
 ORDER BY 
-	input_balance DESC,
-	output_balance DESC,
-	block_date DESC
-	
+    input_balance DESC,
+    output_balance DESC,
+    block_date DESC
