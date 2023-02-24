2e08e96704b72bf8de17c2611a7f14692d3eb285
clementmariebrisson <clement.marie-brisson2.auditeur@lecnam.net>
2023-02-24 01:34:06 +0100

    CMB|20230224|SQLFluff test on models/DTM_.sql

--- dbt/models/dbt-bigquery/DTM_bitcoin_cash_transactions_3M.sql
+++ dbt/models/dbt-bigquery/DTM_bitcoin_cash_transactions_3M.sql
@@ -1,13 +1,13 @@
-{{ config(materialized='table', table_name='DTM_bitcoin_cash_transactions_3M') }}
+{{ config(materialized='table', table_name='DTM_bitcoin_cASh_transactions_3M') }}
 -- THIS REQUEST CREATES THE STAGING TABLE
 SELECT
-	ARRAY_TO_STRING(inputs_addresses, ",") as inputs_addresses,
-	ARRAY_TO_STRING(outputs_addresses, ",") as outputs_addresses,
-	SUM(input_value) as input_balance,
-	SUM(output_value) as output_balance,
-	DATE(block_timestamp) as block_date
-FROM `bitcoin-cash-challenge.dbt_cmariebrisson.STG_bitcoin_cash_transactions_3M`
-WHERE is_coinbase = FALSE
+	ARRAY_TO_STRING(inputs_addresses, ",") AS inputs_addresses,
+	ARRAY_TO_STRING(outputs_addresses, ",") AS outputs_addresses,
+	SUM(input_value) AS input_balance,
+	SUM(output_value) AS output_balance,
+	DATE(block_timestamp) AS block_date
+FROM `bitcoin-cASh-challenge.dbt_cmariebrisson.STG_bitcoin_cASh_transactions_3M`
+WHERE is_coinbASe = FALSE
 GROUP by 
 	inputs_addresses,
 	outputs_addresses,
